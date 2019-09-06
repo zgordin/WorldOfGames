19d029f917f71bddacf1a99027e0f380b8eb8836
zgordin <52931767+zgordin@users.noreply.github.com>
2019-09-06 17:02:55 +0300

    add remark for generate number func

--- GuessGame.py
+++ GuessGame.py
@@ -1,6 +1,7 @@
 import random
 # import score
 
+#Generate number that will return a random number between 1 to difficulty
 def generate_number(difficulty):
     secret_number = random.randint(1, difficulty)
     return  secret_number
