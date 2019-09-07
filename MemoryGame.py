cf8c8cbeba0022da921f1d3df0ed826c11c1f68b
zgordin <52931767+zgordin@users.noreply.github.com>
2019-09-07 11:27:18 +0300

    addind func to mem file

---
+++ MemoryGame.py
@@ -0,0 +1,40 @@
+import random
+import time
+#from utils import screen_cleaner
+#import score
+
+
+def generate_sequence(difficulty):
+    seq_list = list()
+    for i in range(difficulty):
+        num = random.randint(1, 101)
+        seq_list.append(num)
+    return seq_list
+
+
+def get_list_from_user(difficulty):
+    usr_seq_list = list()
+    for i in range(difficulty):
+        num = int(input("Please enter a number between 1-100: "))
+        usr_seq_list.append(num)
+    return usr_seq_list
+
+def is_list_equal(list_a, list_b):
+    if list_a == list_b:
+        return True
+    else:
+        return False
+
+
+def play(difficulty):
+    list_a = generate_sequence(difficulty)
+    print(list_a)
+    time.sleep(0.7)
+    # screen_cleaner()
+    list_b = get_list_from_user(difficulty)
+
+    if is_list_equal(list_a,list_b) == True:
+        print("True")
+        #score.add_score(difficulty)
+    else:
+        print("False")
