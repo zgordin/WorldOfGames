9676ab39fa55583ac67aa1a7ca6426708936bdc0
zgordin <52931767+zgordin@users.noreply.github.com>
2019-09-07 09:42:05 +0300

    fixed message in get guess from user

--- GuessGame.py
+++ GuessGame.py
@@ -9,7 +9,7 @@ def generate_number(difficulty):
 
 # Ask the user to guess a number
 def get_guess_from_user(difficulty):
-    guess = int(input("Please guess the number between 1 and",difficulty," :"))
+    guess = int(input("Please guess the number between 1 and %d(difficulty) :"))
     while guess > difficulty or guess < 1:
         print("Input should be a number between 1 - ",difficulty," \n")
         guess = int(input("Please guess the number: \n"))
@@ -32,7 +32,7 @@ def compare_results(difficulty, secret_number):
 
 def play(difficulty):
     random_number = generate_number(difficulty)
-    user_input = get_guess_from_user()
+    user_input = get_guess_from_user(difficulty)
 
     print(random_number , user_input)
     if compare_results(difficulty,random_number) == True:
