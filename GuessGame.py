2d671ccfb0fabbd2f53bb82685327af5156df635
zgordin <52931767+zgordin@users.noreply.github.com>
2019-09-07 10:10:05 +0300

    fixed compare results function

--- GuessGame.py
+++ GuessGame.py
@@ -1,6 +1,6 @@
 import random
 # import score
-user_input = 0
+
 
 # Generate number that will return a random number between 1 to difficulty
 def generate_number(difficulty):
@@ -9,33 +9,26 @@ def generate_number(difficulty):
 
 # Ask the user to guess a number
 def get_guess_from_user(difficulty):
-    guess = int(input("Please guess the number between 1 and %d(difficulty) :"))
+    guess = int(input("Please guess the number between 1 and %d :"% difficulty))
     while guess > difficulty or guess < 1:
         print("Input should be a number between 1 - ",difficulty," \n")
         guess = int(input("Please guess the number: \n"))
     return guess
 
-def compare_results(difficulty, secret_number):
-    if secret_number == user_input :
+def compare_results(user_input, secret_number):
+    if secret_number == user_input:
         return True
     else:
         return False
-
-
-# Compare the secret generated number to the one prompted
-# def compare_results(generate_number, get_guess_from_user):
-#     if generate_number == get_guess_from_user:
-#         return True
-#     else:
-#         return False
-
 
 def play(difficulty):
     random_number = generate_number(difficulty)
     user_input = get_guess_from_user(difficulty)
 
-    print(random_number , user_input)
-    if compare_results(difficulty,random_number) == True:
+    print(random_number, user_input)
+    #get results comparison
+    result=compare_results(user_input,random_number)
+    if result==True:
         print("True")
         # score.add_score(difficulty)
     else:
